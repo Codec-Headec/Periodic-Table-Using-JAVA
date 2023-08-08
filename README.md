e7fa4ed25aab4c87f1c0d5adad7042e4417edd03
Amey <ameykurade60@gmail.com>
2023-08-08 22:24:12 +0530

    Init

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Periodic-Table-Using-JAVA
+# Welcome to Project 2
+Here I created a program using java to get information about Periodic Table Element.
+
+# Usage
+> Choose 1 for process. <br> <br>
+> Choose 2 to directly end. <br>
+
+After that if you selected 1
+
+> Choose 3 to continue
+
+After this,
+
+>Type as atomic number of which element you want to information (eg. 1, 2, 3, ......, 118.)
+
+```
+Name : Hydrogen <br>
+Symbol : H <br>
+Atomic Number : 1 <br>
+Thanks for using ======> ❤️ 
+```
+
+
+You will get information in the above format.
+
+# Screenshot
+
+<img src="./pics/no.1.png">
---
+++ src/pertab/PerTab.java
@@ -0,0 +1,46 @@
+package pertab;
+
+import java.util.Scanner;
+
+public class PerTab {
+    public static void main(String[] args) {
+        int n, m, a, Exi, Exit;
+
+        System.out.println("Welcome to Modern Periodic Table \n \n");
+
+        System.out.println("> Press 1 to know about any element in Modern Periodic Table");
+        System.out.println("> Press 2 to exit");
+        Scanner sc;
+
+        System.out.println("Choose Option:");
+
+        sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        if (n == 1)
+        {
+            System.out.println("> Press 3 to search element my atomic number");
+            m = sc.nextInt();
+
+            if (m == 3)
+            {
+                System.out.println("Enter the atomic number you want to search:");
+                a = sc.nextInt();
+
+                if (a == 1)
+                {
+                    System.out.println("Name : Hydrogen");
+                    System.out.println("Symbol : H");
+                    System.out.println("Atomic Number : 1");
+                    System.out.println("Thanks for using ======> ❤️");
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Thanks for using ======> ❤️");
+        }
+
+    }
+}
